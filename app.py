5e40f955d50a3586f695417b5d9f110ecc9681b2
Mayuresh Rajendra Yadav <69915267+Mayuresh3@users.noreply.github.com>
2022-11-03 19:29:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,15 @@ import streamlit as st
 st.title("Cloud Computing")
 
 num = st.number_input('Pick a number', 0, 100)
-st.write(num)
+if(num>90):
+    st.write("O")
+elif(num>80):
+    st.write("A+")
+elif(num>70):
+    st.write("A")
+elif(num>60):
+    st.write("B+")
+elif(num>50):
+    st.write("B")
+else:
+    print("Fail")
